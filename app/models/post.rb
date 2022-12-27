6dfa167d2f070cd5ac32bc91d7f28dc395429ec9
Raúl Reymundi <raul@kickstarter.com>
2022-12-26 22:06:16 -0400

    commit tree f22855dccea276d66b8f4311f7cbdf0e8b918667

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,11 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: ['Fiction', 'Non-Fiction'] }
+    validate :clickbait
+
+    def clickbait
+        self.title == "True Facts" ? errors.add(:title, "We don't like clickbait!") : self.title
+    end
 end
